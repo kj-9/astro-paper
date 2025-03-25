a185146b3b5dddeaae8291bbdb8490979b3b1600
Kj <46800811+kj-9@users.noreply.github.com>
2025-03-25 22:35:42 +0900

    feat: enhance post filtering with timezone support

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -1,10 +1,19 @@
 import type { CollectionEntry } from "astro:content";
 import { SITE } from "@/config";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const postFilter = ({ data }: CollectionEntry<"blog">) => {
+  const pubDatetime = dayjs(data.pubDatetime).tz(
+    data.timezone || SITE.timezone
+  );
+
   const isPublishTimePassed =
-    Date.now() >
-    new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
+    Date.now() > pubDatetime.millisecond() - SITE.scheduledPostMargin;
   return !data.draft && (import.meta.env.DEV || isPublishTimePassed);
 };
 
